97d4498060b6806e8ed5c995ea1ca2d7efd11dea
FFCCN <152023102+Rostistav@users.noreply.github.com>
2023-12-03 11:22:03 +0300

    Update test3.cpp

--- test3.cpp
+++ test3.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
-int main() {
-  std::cout << "Hello, world!";
+int main(){
 
-  return 0;
+    int number;
+
+    std::cout << "Enter number 1 or 0" << std::endl;
+    std::cin >> number;
+
+    if (number == 1) {
+      std::cout << "This is true";
+    } else if  (number == 0) {
+      std::cout << "This is false";
+    } else {
+      std::cout << "These are not numbers 1 or 0";
+    }
+        return 0;
 }
